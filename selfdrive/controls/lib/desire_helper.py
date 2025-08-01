d6a8e19479098c6a12b61be8171570eb0efc6c67
pkocyk123 <141670809+pkocyk123@users.noreply.github.com>
2025-07-31 19:34:25 -0500

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -9,7 +9,7 @@ LaneChangeState = log.LaneChangeState
 LaneChangeDirection = log.LaneChangeDirection
 
 LANE_CHANGE_SPEED_MIN = 20 * CV.MPH_TO_MS
-LANE_CHANGE_TIME_MAX = 12.
+LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
   LaneChangeDirection.none: {
@@ -152,7 +152,7 @@ class DesireHelper:
       # LaneChangeState.laneChangeFinishing
       elif self.lane_change_state == LaneChangeState.laneChangeFinishing:
         # fade in laneline over 1s
-        self.lane_change_ll_prob = min(self.lane_change_ll_prob + 0.4 * DT_MDL, 1.0)
+        self.lane_change_ll_prob = min(self.lane_change_ll_prob + 0.3 * DT_MDL, 1.0)
 
         if self.lane_change_ll_prob > 0.99:
           self.lane_change_direction = LaneChangeDirection.none
